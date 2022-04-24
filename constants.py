0d718ad8370f6e0be5114698092439ba7c2bed72
Manuel <mgonzalez4648@gmail.com>
2022-04-24 11:53:06 +0200

    Station retrieval working

---
+++ constants.py
@@ -0,0 +1,8 @@
+BASE_URL = 'https://muybici.es/api-labici.php'
+FILTERS = {
+    'module': 'parking',
+    'method': 'get-locations',
+    'city': 'MU'
+}
+BIKE_NUM_ATTRIBUTE = 'xocupados'
+STOP_NAME_ATTRIBUTE = 'descripcion'
--- run.py
+++ run.py
@@ -1,5 +1,31 @@
+from requests import get
+from constants import BASE_URL, FILTERS, BIKE_NUM_ATTRIBUTE, STOP_NAME_ATTRIBUTE
+from env import REF_POINT
+from helpers import fixed_point_distance
+
 print('Requesting bike stops within its occupation...')
+res = get(BASE_URL, params=FILTERS)
 
-print('Filtering stops with no bikes...')
+if res.status_code != 200:
+    print(
+        f'Request failed. MuyBici server responsed with code {res.status_code} ☹️')
+    exit(1)
 
-print('Ordering by distance to a fixed point (lat, lon)...')
+print('Filtering stops with bikes...')
+stops = res.json()['data']
+stops_with_bikes = [stop for stop in stops if int(
+    stop[BIKE_NUM_ATTRIBUTE]) > 0]
+
+print('Ordering by distance to a fixed point...')
+stops_ordered = sorted(stops_with_bikes,
+                       key=lambda stop: fixed_point_distance(
+                           stop,
+                           REF_POINT
+                       ))
+
+print()
+print('The closest three stops with bikes are:')
+print('********************************************************')
+for stop in stops_ordered[:3]:
+    print(f'\t{stop[STOP_NAME_ATTRIBUTE]}: {stop[BIKE_NUM_ATTRIBUTE]} bikes 🚲')
+print('********************************************************')
